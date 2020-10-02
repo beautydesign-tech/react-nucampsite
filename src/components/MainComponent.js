b250c1b7fbae6f5067aba214a78022b6ab735fc2
extraordinaryadventurer <46338886+extraordinaryadventurer@users.noreply.github.com>
2020-10-02 17:39:31 -0400

    React Transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -15,6 +15,7 @@ import {
   fetchComments,
   fetchPromotions,
 } from "../redux/ActionCreators";
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = (state) => {
   return {
@@ -86,22 +87,26 @@ class Main extends Component {
     return (
       <div>
         <Header />
+        <TransitionGroup>
+          <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
         <Switch>
-          <Route path="/home" component={HomePage} />
-          <Route
-            exact
-            path="/directory"
-            render={() => <Directory campsites={this.props.campsites} />}
-          />
-          <Route path="/directory/:campsiteId" component={CampsiteWithId} />
+            <Route path="/home" component={HomePage} />
+            <Route
+              exact
+              path="/directory"
+              render={() => <Directory campsites={this.props.campsites} />}
+            />
+            <Route path="/directory/:campsiteId" component={CampsiteWithId} />
           <Route exact path="/contactus" component={Contact} />
-          <Route
-            exact
-            path="/aboutus"
-            render={() => <About partners={this.props.partners} />}
-          />
-          <Redirect to="/home" />
-        </Switch>
+            <Route
+              exact
+              path="/aboutus"
+              render={() => <About partners={this.props.partners} />}
+            />
+            <Redirect to="/home" />
+          </Switch>
+          </CSSTransition>
+        </TransitionGroup>
         <Footer />
       </div>
     );
