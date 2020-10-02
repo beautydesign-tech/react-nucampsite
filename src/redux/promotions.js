f29c3930f7c478c578c046a6596370e9a04ec199
extraordinaryadventurer <46338886+extraordinaryadventurer@users.noreply.github.com>
2020-10-02 09:47:34 -0400

    Intro to Redux

---
+++ src/redux/promotions.js
@@ -0,0 +1,28 @@
+import * as ActionTypes from "./ActionTypes";
+
+export const Promotions = (
+  state = {
+    isLoading: true,
+    errMess: null,
+  },
+  action
+) => {
+  switch (action.type) {
+    case ActionTypes.ADD_PROMOTIONS:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        promotions: action.payload,
+      };
+
+    case ActionTypes.PROMOTIONS_LOADING:
+      return { ...state, isLoading: true, errrMess: null, promotions: [] };
+
+    case ActionTypes.PROMOTIONS_FAILED:
+      return { ...state, isLoading: false, errMess: action.payload };
+
+    default:
+      return state;
+  }
+};
